0a3a197f36cfa7147f5b2f6cdbea177e026de060
Phil Simmons <phil@tubbycatgames.com>
2020-01-28 00:16:43 -0500

    Removed boilerplate from Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   plugins: ["gatsby-plugin-stylus"]
-  /* Your site config here */
 }
