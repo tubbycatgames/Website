ac677c9d16ec992ee20c333777bc60a3e076890d
Phil Simmons <phil@tubbycatgames.com>
2020-01-29 22:43:33 -0500

    Narrowed down social list while content gets built out

--- src/components/social-bar.js
+++ src/components/social-bar.js
@@ -1,31 +1,31 @@
 import React from "react"
 
-import Discord from "../images/social/Discord.png"
+// import Discord from "../images/social/Discord.png"
 import Facebook from "../images/social/Facebook.png"
-import GameJolt from "../images/social/GameJolt.png"
+// import GameJolt from "../images/social/GameJolt.png"
 import Instagram from "../images/social/Instagram.png"
-import Itch from "../images/social/Itch.png"
+// import Itch from "../images/social/Itch.png"
 import Twitter from "../images/social/Twitter.png"
-import YouTube from "../images/social/YouTube.png"
+// import YouTube from "../images/social/YouTube.png"
 
 import styles from "./social-bar.module.styl"
 
 const socialData = [
-  {
-    alt: "Discord Social Media Icon",
-    href: "https://discord.gg/SDRavf",
-    src: Discord,
-  },
-  {
-    alt: "Itch Page Icon",
-    href: "https://itch.io/profile/tubbycatgames",
-    src: Itch,
-  },
-  {
-    alt: "Game Jolt Page Icon",
-    href: "https://gamejolt.com/@tubbycatgames",
-    src: GameJolt,
-  },
+  // {
+  //   alt: "Discord Social Media Icon",
+  //   href: "https://discord.gg/SDRavf",
+  //   src: Discord,
+  // },
+  // {
+  //   alt: "Itch Page Icon",
+  //   href: "https://itch.io/profile/tubbycatgames",
+  //   src: Itch,
+  // },
+  // {
+  //   alt: "Game Jolt Page Icon",
+  //   href: "https://gamejolt.com/@tubbycatgames",
+  //   src: GameJolt,
+  // },
   {
     alt: "Instagram Social Media Icon",
     href: "https://instagram.com/tubbycatgames",
@@ -41,11 +41,11 @@ const socialData = [
     href: "https://fb.me/tubbycatgames",
     src: Facebook,
   },
-  {
-    alt: "YouTube Channel Icon",
-    href: "https://www.youtube.com/channel/UC_6l3SdhVjdtTd2DnHK2JbQ",
-    src: YouTube,
-  },
+  // {
+  //   alt: "YouTube Channel Icon",
+  //   href: "https://www.youtube.com/channel/UC_6l3SdhVjdtTd2DnHK2JbQ",
+  //   src: YouTube,
+  // },
 ]
 
 const SocialIcon = ({ alt, href, src }) => {
