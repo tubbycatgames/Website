08b945bd247ca3d5c9ac1dbd3fa61248eadeb1ed
Phil Simmons <phil@tubbycatgames.com>
2020-04-23 18:35:06 -0400

    Added Custom 404 Page and Remove AWS Deploy in light of Netlify switch

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+import { Layout } from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Page Not Found!</h1>
+    <h2>Use the navigation tabs above to find the page you wanted</h2>
+  </Layout>
+)
