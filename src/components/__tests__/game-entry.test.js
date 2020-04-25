69659c2fe9b19ccb43b1b52e197edfe1cbca578b
Phil Simmons <phil@tubbycatgames.com>
2020-04-25 00:14:18 -0400

    Expanded Test Coverage for Game Entries

--- src/components/__tests__/game-entry.test.js
+++ src/components/__tests__/game-entry.test.js
@@ -3,19 +3,19 @@ import renderer from "react-test-renderer"
 
 import GameEntry from "../game-entry"
 
+const icon = {
+  childImageSharp: {
+    fluid: {
+      aspectRatio: 1,
+      sizes: "(max-width: 300px) 100vw, 300px",
+      src: "image",
+      srcSet: "image2",
+    },
+  },
+}
+
 describe("Game Entry", () => {
   it("renders a game entry for a game listing", () => {
-    const icon = {
-      childImageSharp: {
-        fluid: {
-          aspectRatio: 1,
-          sizes: "(max-width: 300px) 100vw, 300px",
-          src: "image",
-          srcSet: "image2",
-        },
-      },
-    }
-
     const tree = renderer.create(
       <GameEntry
         creation="2004-09-22"
@@ -26,6 +26,19 @@ describe("Game Entry", () => {
         slug="/my/internal/link.html"
         source="github.com"
         video="youtube.com"
+      />
+    )
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it('should omit optional fields', () => {
+    const tree = renderer.create(
+      <GameEntry
+        creation="2004-09-22"
+        description="What a game!"
+        icon={icon}
+        name="Da Game"
+        slug="/my/internal/link.html"
       />
     )
     expect(tree.toJSON()).toMatchSnapshot()
