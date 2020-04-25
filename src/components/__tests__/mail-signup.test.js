a9f07345d136882a9c18494956110c2ebf1648fe
Phil Simmons <phil@tubbycatgames.com>
2020-04-25 00:46:01 -0400

    Rewrote Mail Signup for Testability

--- src/components/__tests__/mail-signup.test.js
+++ src/components/__tests__/mail-signup.test.js
@@ -1,11 +1,21 @@
 import React from "react"
 import renderer from "react-test-renderer"
 
-import MailSignup from "../mail-signup"
+import MailSignup, { MailForm } from "../mail-signup"
 
 describe("Mail Signup", () => {
   it("renders a mailing list signup form", () => {
     const tree = renderer.create(<MailSignup />)
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it("renders a mailing form with an error", () => {
+    const tree = renderer.create(<MailForm error={"Ooops"} />)
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it("renders a mailing form with a result", () => {
+    const tree = renderer.create(<MailForm result={"Worked!"} />)
     expect(tree.toJSON()).toMatchSnapshot()
   })
 })
--- src/components/mail-signup.js
+++ src/components/mail-signup.js
@@ -3,6 +3,34 @@ import addToMailChimp from "gatsby-plugin-mailchimp"
 
 import styles from "./mail-signup.module.styl"
 
+export const MailForm = ({ email, error, onEmailChange, onSubmit, result }) => {
+  const errorText = error ? <p className={styles.error}>{error}</p> : null
+  const resultText = result ? <p className={styles.result}>{result}</p> : null
+
+  return (
+    <div>
+      <h2 className={styles.header}>Join our mailing list for game updates!</h2>
+      {errorText}
+      {resultText}
+      <form className={styles.form} onSubmit={onSubmit}>
+        <input
+          aria-label="Mailing List Signup Email"
+          className={styles.input}
+          name="email"
+          onChange={onEmailChange}
+          placeholder="Enter your email here"
+          required
+          type="email"
+          value={email}
+        />
+        <button className={styles.button} type="submit">
+          Sign up!
+        </button>
+      </form>
+    </div>
+  )
+}
+
 export default class MailSignup extends Component {
   state = {
     email: "",
@@ -26,45 +54,22 @@ export default class MailSignup extends Component {
     if (response.result === "success") {
       this.setState({ error: "", result: response.msg })
     } else if (response.result === "error") {
-      if (response.msg.includes("is already subscribed")) {
-        this.setState({ error: "You are already subscribed!", result: "" })
-      } else {
-        this.setState({ error: response.msg, result: "" })
-      }
+      const errorText = response.msg.includes("is already subscribed")
+        ? "You are already subscribed!"
+        : response.msg
+      this.setState({ error, result: "" })
     }
   }
 
   render() {
-    const error = this.state.error ? (
-      <p className={styles.error}>{this.state.error}</p>
-    ) : null
-    const result = this.state.result ? (
-      <p className={styles.result}>{this.state.result}</p>
-    ) : null
-
     return (
-      <div>
-        <h2 className={styles.header}>
-          Join our mailing list for game updates!
-        </h2>
-        {error}
-        {result}
-        <form className={styles.form} onSubmit={this.handleSubmit}>
-          <input
-            aria-label="Mailing List Signup Email"
-            className={styles.input}
-            name="email"
-            onChange={this.handleEmailChange}
-            placeholder="Enter your email here"
-            required
-            type="email"
-            value={this.state.email}
-          />
-          <button className={styles.button} type="submit">
-            Sign up!
-          </button>
-        </form>
-      </div>
+      <MailForm
+        email={this.state.email}
+        error={this.state.error}
+        onEmailChange={this.handleEmailChange}
+        onSubmit={this.handleSubmit}
+        result={this.state.result}
+      />
     )
   }
 }
