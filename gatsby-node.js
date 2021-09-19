fbf15950ae12591ca91d854ea30713d7b957fef8
Phil Simmons <phil@tubbycatgames.com>
2021-09-19 11:09:16 -0500

    Removed Dependency on React Hot Loader

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,15 +51,3 @@ exports.onCreateNode = ({ actions, getNode, node }) => {
     }
   }
 }
-
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  if (stage.startsWith("develop")) {
-    actions.setWebpackConfig({
-      resolve: {
-        alias: {
-          "react-dom": "@hot-loader/react-dom",
-        },
-      },
-    })
-  }
-}
